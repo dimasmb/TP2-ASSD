ddd33aa5972182e76ab611d7a53afc53a6502583
Shalycht <pdalla@itba.edu.ar>
2022-05-09 11:29:58 -0300

    added the midi file and some useful libraries

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 # Project modules
+import mido
+import numpy as np
+import pandas as pd
+from mido import MidiFile
+
 from src.app import main
 
 if __name__ == "__main__":
     main()
+
+# carga de archivo y visualización de la estructura
+archivo_midi = MidiFile('concierto_de_aranjuez.mid')
+print('info', archivo_midi)
+print('length', archivo_midi.length)
+print('charset', archivo_midi.charset)
+print('ticks_per_beat', archivo_midi.ticks_per_beat)
