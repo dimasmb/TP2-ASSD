2800ddbea693190e72f44526b40ba6e056071117
Dimas <dbosch@itba.edu.ar>
2022-05-10 16:29:16 -0300

    Aparecen los tracks. Falta pulir el guardado

--- main.py
+++ main.py
@@ -8,10 +8,3 @@ from src.app import main
 
 if __name__ == "__main__":
     main()
-
-# carga de archivo y visualización de la estructura
-# archivo_midi = MidiFile('concierto_de_aranjuez.mid')
-# print('info', archivo_midi)
-# print('length', archivo_midi.length)
-# print('charset', archivo_midi.charset)
-# print('ticks_per_beat', archivo_midi.ticks_per_beat)
--- src/mainwindow.py
+++ src/mainwindow.py
@@ -1,8 +1,13 @@
 # PyQt5 modules
-from PyQt5.QtWidgets import QMainWindow, QFileDialog
+from PyQt5.QtWidgets import QMainWindow, QFileDialog, QHBoxLayout, QLabel, QSlider
+from PyQt5 import QtCore
 
 # Project modules
 from src.ui.mainwindow import Ui_Sintetizador
+
+# Utilities
+import mido
+from midi2audio import FluidSynth
 
 
 class MainWindow(QMainWindow, Ui_Sintetizador):
@@ -10,14 +15,54 @@ class MainWindow(QMainWindow, Ui_Sintetizador):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
-        # self.setupBtns()
+        self.setupBtns()
+        self.midi_file = mido.MidiFile()
+        self.midi_filename=""
+        self.tracks_sliders=[]
 
-    # def setupBtns(self):
-    #     self.pushButton_Abrir.clicked.connect(self.AbrirClicked)
-    #
-    # def AbrirClicked(self):
-    #     fname = QFileDialog.getOpenFileName(self, "Open file", "", "MIDI(*.mid)")
-    #     self.line_Examinar.setText(fname[0])
-    #
-    # def GuardarClicked(self):
-    #     pass
+    def setupBtns(self):
+        self.pushButton_Abrir.clicked.connect(self.AbrirClicked)
+        self.pushButton_Guardar.clicked.connect(self.GuardarClicked)
+
+    def AbrirClicked(self):
+        self.midi_filename, type_midi = QFileDialog.getOpenFileName(self, "Open file", "", "MIDI(*.mid)")
+        if self.midi_filename!="":
+            self.RecieveFile()
+
+    def GuardarClicked(self):
+        save_filename, save_type = QFileDialog.getSaveFileName(self, "Save file", "", "MIDI(*.mid);;WAV(*.wav);;MP·(*.mp3)")
+        # print(save_filename)
+        if save_filename != "":
+            if save_type=="MIDI(*.mid)":
+                file.save('newsong.mid')
+            elif save_type=="WAV(*.wav)":
+                fs = FluidSynth(sound_font='font.sf2')
+                fs.midi_to_audio(self.midi_filename, save_filename)
+            elif save_type == "MP3(*.mp3)":
+                fs = FluidSynth(sound_font='font.sf2')
+                fs.midi_to_audio(self.midi_filename, save_filename)
+
+    def RecieveFile(self):
+        self.midi_file = mido.MidiFile(self.midi_filename)
+        self.clearTracks()
+        for i, track in enumerate(self.midi_file.tracks):
+            self.tracks_sliders.append(self.buildTrack(i, track.name))
+
+    def buildTrack(self, n, track_name):
+        horizontalLayout = QHBoxLayout()
+
+        label = QLabel(self.scrollAreaWidgetContents)
+        label.setText(str(n) + ". " +track_name)
+        horizontalLayout.addWidget(label)
+
+        horizontalSlider = QSlider(self.scrollAreaWidgetContents)
+        horizontalSlider.setOrientation(QtCore.Qt.Horizontal)
+        horizontalLayout.addWidget(horizontalSlider)
+
+        self.verticalLayout_3.addLayout(horizontalLayout)
+        return horizontalSlider
+
+    def clearTracks(self):
+        for i in reversed(range(self.verticalLayout_3.count())):
+            for a in reversed(range(self.verticalLayout_3.itemAt(i).count())):
+                self.verticalLayout_3.itemAt(i).itemAt(a).widget().setParent(None)
